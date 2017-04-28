dc224b7f068452e4b07a3c2540112e323d3109fa
LenaKuchko <kuchkoel@gmail.com>
2017-04-28 12:41:18 -0700

    add pink font and content under the main button

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
 $(function() {
+  $("#start-btn").click(function () {
+    $("#form-info").show();
+    $(".main-header").hide();
+  });
   $("#form-info").submit(function (event) {
     event.preventDefault();
     var quest1 = $("#quest1").val();
@@ -9,20 +13,26 @@ $(function() {
 
     if (quest1=="css" && quest5=="css" && quest4=="css" && quest3!=="c") {
       console.log("Your track is totally css");
-    } else if (quest1=="css" && quest2=="java" && (quest4=="css" || quest5=="java") && quest3!=="c") {
-      console.log("CSS or java");
-    } else if ((quest1=="css" || quest5=="java") && quest3!=="c") {
+    } else if (quest1=="css" && quest3=="java") {
       console.log("Java or css");
-    } else if (quest3=="c" && quest1=="c" && quest5=="c") {
+    } else if (quest1=="css" && quest3=="css") {
+      console.log("Css!");
+    } else if (quest3=="c" && quest1=="c" && quest2=="c") {
       console.log("Totally c");
-    } else if (quest4=="java" || quest1=="c" && quest3=="c") {
-      console.log("C or Java back-end");
-    } else if ((quest3=="c" || quest4=="java") && quest1!=="css") {
+    } else if ((quest3=="java" && (quest2=="c" || quest5=="java")) && quest1 !=="css") {
+      console.log("Java mobile");
+    } else if ((quest3=="c" || quest4=="java") && quest1!=="css" && quest3!=="css") {
       console.log("java or c");
-    } else if (quest1=="java" && quest3=="java") {
+    } else if ((quest3=="c" || quest4=="java") && quest3!=="css" && quest2=="c") {
+      console.log("C or java");
+    }else if (quest3=="java"&& quest2=="java"&&quest1!=="css") {
       console.log("java mobile");
-    } else if (quest3=="java" && quest5=="java" ||quest1=="c")) {
+    } else if (quest3=="java" && (quest5=="java" || quest1=="c")) {
       console.log("java c");
+    } else if (quest1=="css" && quest3=="c") {
+      console.log("You are art person with big ambission) your track Css Some industrial companies needs css desners.");
+    } else if (quest3=="css") {
+      console.log("css too");
     }
   });
 });
